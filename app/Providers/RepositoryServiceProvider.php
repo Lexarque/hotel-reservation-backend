ca75bc7995397bf9d0b90f883e2b5fc7e79dac9e
Lexarque <satrialdyfarizki06@gmail.com>
2023-03-16 09:50:08 +0700

    Add user interface repo to provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Auth\AuthRepository;
+use App\Repositories\User\UserRepository;
 use App\Repositories\Auth\AuthRepositoryInterface;
+use App\Repositories\User\UserRepositoryInterface;
 use App\Repositories\HotelRoom\HotelRoomRepository;
 use App\Repositories\HotelRoom\HotelRoomTypeRepository;
 use App\Repositories\BookingOrder\BookingOrderRepository;
@@ -19,6 +21,7 @@ class RepositoryServiceProvider extends ServiceProvider
         HotelRoomRepositoryInterface::class => HotelRoomRepository::class,
         HotelRoomTypeRepositoryInterface::class => HotelRoomTypeRepository::class,
         BookingOrderRepositoryInterface::class => BookingOrderRepository::class,
+        UserRepositoryInterface::class => UserRepository::class,
     ];
 
     /**
